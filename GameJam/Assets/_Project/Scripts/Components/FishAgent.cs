6df912c1ca69c5954a1209f1883f368c1a7f63f3
Julius de Blaaij <julius.de.blaaij@gmail.com>
2019-04-25 11:04:46 +0200

    Added WIP FishAgent script, Fish prite 01

---
+++ GameJam/Assets/_Project/Scripts/Components/FishAgent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishAgent : MonoBehaviour
+{
+    public Transform player;
+
+
+
+    void Update()
+    {
+        
+    }
+}
