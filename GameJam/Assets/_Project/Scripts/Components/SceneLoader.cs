66d1118ec0016f2eb76ed598b16e6a1908414b29
Julius de Blaaij <julius.de.blaaij@gmail.com>
2019-04-25 12:44:17 +0200

    Added GameOver, SceneLoader

---
+++ GameJam/Assets/_Project/Scripts/Components/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectsWithTag("Fish").Length == 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
