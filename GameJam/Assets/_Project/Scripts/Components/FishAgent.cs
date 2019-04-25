34ad946e605b19164ed3c17ec05c273b9cefa696
M@@ndag1 <hofland.casey@gmail.com>
2019-04-25 12:18:42 +0200

    Fish Update

--- GameJam/Assets/_Project/Scripts/Components/FishAgent.cs
+++ GameJam/Assets/_Project/Scripts/Components/FishAgent.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FishAgent : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class FishAgent : MonoBehaviour
 {
-    public Transform player;
+    [SerializeField] protected float speed = 1;
 
+    protected Transform target;
+    protected Transform planet;
+    protected new Rigidbody2D rigidbody2D;
 
-
-    void Update()
+    protected void Awake()
     {
-        
+        GameObject tar = GameObject.FindGameObjectWithTag("Player");
+        if (tar) target = tar.transform;
+
+        GameObject plan = GameObject.FindGameObjectWithTag("Planet");
+        if (plan) planet = plan.transform;
+
+        /*
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        planet = GameObject.FindGameObjectWithTag("Planet").transform;
+        */
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    protected void Move()
+    {
+        Vector2 movement = transform.up * speed * Time.deltaTime;
+
+        rigidbody2D.position += movement;
     }
 }
--- GameJam/Assets/_Project/Scripts/Components/Rotator.cs
+++ GameJam/Assets/_Project/Scripts/Components/Rotator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlanetRotator : MonoBehaviour
+public class Rotator : MonoBehaviour
 {
     public float rotationSpeed = 1;
 
